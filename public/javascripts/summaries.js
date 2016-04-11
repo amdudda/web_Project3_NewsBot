2780c151b096556258bb337eecc2c1808997c194
amdudda <tk0654wm@minneapolis.edu>
2016-04-11 09:25:10 -0500

    add mouseover/out events to each article to show/hide summary

--- public/javascripts/summaries.js
+++ public/javascripts/summaries.js
@@ -7,17 +7,16 @@ var allSummaries = document.getElementsByClassName("articleSummary");
 for (var i=0; i<allSummaries.length; i++){
 	var mySummary = allSummaries[i];
 	var recID = mySummary.id.substring(2); 
-	//console.log(recID);
-
+	
 	// get the corresponding a tag, which has an ID matching the record id
 	var link = document.getElementById(recID);
-	console.log(link.id);
 	// add the event listeners to it
 	// TODO for some reason, each new event listener supersedes/replaces all the ones before it.
 	// why isn't it adding a new event listener with a new tag for each one?
 	link.addEventListener("mouseover", function(){ 
-		console.log("moved in over " + recID);
-		mySummary.style.display = "block"; 
+		document.getElementById("as"+this.id).style.display = "block"; 
 	} );
-	link.addEventListener("mouseout", function(){ mySummary.style.display = "none"; } );
+	link.addEventListener("mouseout", function(){ 
+		document.getElementById("as"+this.id).style.display = "none"; 
+	} );
 }
